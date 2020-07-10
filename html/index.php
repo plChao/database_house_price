69227625493cd8479e546f8feb3592bb23dd9ab9
sleeping_sheep <poseidon31418926.cs06@g2.nctu.edu.tw>
2020-07-10 21:21:44 +0800

    update html-static/search.html.

--- html/index.php
+++ html/index.php
@@ -6,6 +6,7 @@ if(!isset($_SESSION['userid'])){ header("Location: ./login.php"); }
 
 <!DOCTYPE html>
 <head>
+	<title>Hp-Home</title>
 	<style>
         h2 {text-align: center;}
         p {text-align: center;}
@@ -13,19 +14,20 @@ if(!isset($_SESSION['userid'])){ header("Location: ./login.php"); }
 	</style>
 </head>
 <body>
-<h2>Homepage of DataBase house project</h2>
-<?php
-//session_start();
-//$_SESSION['userid'] = 10;
-//isset($_SESSION['userid']);
-if(isset($_SESSION['userid'])){
-	echo 'Your name : ';
-	echo $_SESSION['userid'];
-	echo '<br>';
-        echo '<br> <a href="http://127.0.0.1/logout.php">logout</a>';
+	<h2>Homepage of DataBase house project</h2>
+	<div>
+		<?php
+		//session_start();
+		//$_SESSION['userid'] = 10;
+		//isset($_SESSION['userid']);
+		if(isset($_SESSION['userid'])){
+			echo 'Your name : ';
+			echo $_SESSION['userid'];
+			echo '<br>';
+				echo '<br> <a href="http://127.0.0.1/logout.php">logout</a>';
 
-}
-?>
-
+		}
+		?>
+	</div>
 </body>
 </html>
