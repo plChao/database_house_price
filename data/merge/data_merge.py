c241cf55fd013a10c37a3c0a0c8f55fdd2eead9b
sleeping_sheep <poseidon31418926.cs06@g2.nctu.edu.tw>
2020-07-12 17:28:47 +0800

    Update python file.

--- data/merge/data_merge.py
+++ data/merge/data_merge.py
@@ -15,11 +15,28 @@ import numpy as np
 
 df = pd.read_csv('../original/107_1/A_lvr_land_A.csv')
 df.drop([0], inplace=True)
-merge_df = pd.DataFrame(columns=df.columns)
+col = list(df.columns)
+col.insert(0, 'city')
+merge_df = pd.DataFrame(columns=col)
 print(merge_df)
 
 
 # In[3]:
+
+
+city_dict = {
+    'A':'台北市', 'J':'新竹縣', 'S':'高雄縣', 
+    'B':'台中市', 'K':'苗栗縣', 'T':'屏東縣', 
+    'C':'基隆市', 'L':'台中縣', 'U':'花蓮縣', 
+    'D':'台南市', 'M':'南投縣', 'V':'台東縣', 
+    'E':'高雄市', 'N':'彰化縣', 'W':'金門縣', 
+    'F':'台北縣', 'O':'新竹市', 'X':'澎湖縣', 
+    'G':'宜蘭縣', 'P':'雲林縣', 'Y':'陽明山', 
+    'H':'桃園縣', 'Q':'嘉義縣', 'Z':'連江縣', 
+    'I':'嘉義市', 'R':'台南縣'}
+
+
+# In[4]:
 
 
 for foldername in os.listdir("../original"):
@@ -29,10 +46,11 @@ for foldername in os.listdir("../original"):
                 tmp_df = pd.read_csv('../original/107_1/' + filename)
                 tmp_df.drop([0], inplace=True)
 #                 print(merge_df.shape, tmp_df.shape)
+                tmp_df['city'] = city_dict[filename[0]]
                 merge_df = merge_df.append(tmp_df, ignore_index=True)
 
 
-# In[4]:
+# In[5]:
 
 
 merge_df.to_csv('merge.csv', index=False, na_rep='NULL')
