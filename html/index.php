9075a7caae051730526b05d896c044a00774603a
sleeping_sheep <poseidon31418926.cs06@g2.nctu.edu.tw>
2020-07-13 14:20:26 +0800

    Modify index.php.

--- html/index.php
+++ html/index.php
@@ -5,28 +5,45 @@ if(!isset($_SESSION['userid'])){ header("Location: ./login.php"); }
 
 
 <!DOCTYPE html>
-<head>
-	<title>Hp-Home</title>
-	<style>
-        h2 {text-align: center;}
-        p {text-align: center;}
-        div {text-align: center;}
-	</style>
-</head>
 <body>
-	<h2>Homepage of DataBase house project</h2>
-	<div>
-		<?php
-		//session_start();
-		//$_SESSION['userid'] = 10;
-		//isset($_SESSION['userid']);
-		if(isset($_SESSION['userid'])){
-			echo 'Your name : ';
-			echo $_SESSION['userid'];
-			echo '<br>';
-				echo '<br> <a href="http://127.0.0.1/logout.php">logout</a>';
-		}
-		?>
-	</div>
+
+<!--<?php
+//session_start();
+//$_SESSION['userid'] = 10;
+//isset($_SESSION['userid']);
+if(isset($_SESSION['userid'])){
+	echo 'Your name : ';
+	echo $_SESSION['userid'];
+	echo '<br>';
+        echo '<br> <a href="http://127.0.0.1/test_project/logout.php">logout</a>';
+
+}
+?>-->
+<div style="text-align:center;"><h1>首頁</h1></div>
+<div style="text-align:right;"><p>
+使用者:
+<?php
+//session_start();
+//$_SESSION['userid'] = 10;
+//isset($_SESSION['userid']);
+if(isset($_SESSION['userid'])){
+        echo $_SESSION['userid'];
+        echo '<br>';
+        echo '<br> <a href="http://127.0.0.1/test_project/logout.php">logout</a>';
+
+}
+?>
+</p></div>
+
+<div style="text-align:center;">
+  <form action="./search.php">
+    <input type="submit" value="搜尋房價" />
+  </form>
+</div>
+<div style="text-align:center;">
+  <form action="./history.php">
+    <input type="submit" value="搜尋紀錄" />
+  </form>
+</div>
 </body>
 </html>
